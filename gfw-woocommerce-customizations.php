0dbf5058930ef98963e86af77230ef1736b1134a
Alex Moise <alex@moise.pro>
2020-02-16 15:14:49 +0200

    Create gfw-woocommerce-customizations.php

---
+++ gfw-woocommerce-customizations.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Plugin Name: GFW Woocommerce customizations
+ * Plugin URI: https://github.com/alexmoise/gfw-woocommerce-customizations
+ * GitHub Plugin URI: https://github.com/alexmoise/gfw-woocommerce-customizations
+ * Description: A custom plugin to add required customizations to Girlfridayweddings Woocommerce shop and to style the front end as required. For details/troubleshooting please contact me at <a href="https://moise.pro/contact/">https://moise.pro/contact/</a>
+ * Version: 0.1
+ * Author: Alex Moise
+ * Author URI: https://moise.pro
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {	exit(0);}
+
+
+
+
+
+
+
+
+
+
+
+
+?>
